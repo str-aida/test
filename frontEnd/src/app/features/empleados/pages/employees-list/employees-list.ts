57c16aade229cfdf09a8adeab72d0c59de56f661
jazmin1626 <luciapavonn@gmail.com>
2026-09-07 02:37:53 -0300

    mejoras en apartado de personal

--- frontEnd/src/app/features/empleados/pages/employees-list/employees-list.ts
+++ frontEnd/src/app/features/empleados/pages/employees-list/employees-list.ts
@@ -1,6 +1,6 @@
 import { Component, inject, ViewChild} from '@angular/core';
 import { EmployeesTableComponent } from '../../components/employees-table/employees-table';
-import { LucideAlertTriangle, LucidePlus, LucideShapes, LucideX } from '@lucide/angular';
+import { LucideTriangleAlert, LucidePlus, LucideUsers, LucideX } from '@lucide/angular';
 import { Employee } from '../../../../core/models/user-profile-response';
 import { EmployeeFormComponent } from '../../components/employee-form/employee-form';
 import { EmployeesService } from '../../../../core/services/employees.service';
@@ -9,7 +9,7 @@ import { Estado } from '../../../../core/models/enums/estado.enum';
 
 @Component({
   selector: 'app-employees-list',
-  imports: [EmployeesTableComponent, EmployeeFormComponent, LucideShapes, LucidePlus, LucideX, LucideAlertTriangle],
+  imports: [EmployeesTableComponent, EmployeeFormComponent, LucideUsers, LucidePlus, LucideX, LucideTriangleAlert],
   templateUrl: './employees-list.html',
   styleUrl: './employees-list.scss',
 })
